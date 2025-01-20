59b36e6ae812b5bf6925372dcaf5a7e329b2f432
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-01-20 00:18:00 +0000

    CNY2025Goals: tweak coin spawning positions

--- src/cny2025/rules/cny2025-goals.js
+++ src/cny2025/rules/cny2025-goals.js
@@ -26,12 +26,45 @@ export default class CNY2025Goals extends Rule {
     const app = this._app
     const hero = app.hero
 
-    const coin = new Coin(app, 3, 3) 
+    // Figure out where to spawn the coin.
+
+    // First, consider all possible ranges in the map.
+    const mapEdgeBuffer = 2
+    const minCol = 0 + mapEdgeBuffer
+    const maxCol = app.gameMap.width - 1 - mapEdgeBuffer
+    const minRow = 0 + mapEdgeBuffer
+    const maxRow = app.gameMap.height - 1 - mapEdgeBuffer
+
+    // Next, pick a random location in the range. 
+    // If the location is too close to the hero, pick another.
+    let col, row
+    do {
+      col = Math.floor(Math.random() * (maxCol - minCol) + minCol)
+      row = Math.floor(Math.random() * (maxRow - minRow) + minRow)
+    } while (isTooCloseToHero(col, row, hero))
+
+    const coin = new Coin(app, col, row) 
     app.addEntity(coin)
   }
 
   increaseScore () {
     this.score++
-    // this.spawnCoin()
+    this.spawnCoin()
   }
+}
+
+function isTooCloseToHero (col, row, hero) {
+  if (col === undefined || row === undefined || !hero) return false
+
+  const tooCloseRange = 5
+  if (
+    col >= hero.col - tooCloseRange
+    && col <= hero.col + tooCloseRange
+    && row >= hero.row - tooCloseRange
+    && row <= hero.row + tooCloseRange
+  ) {
+    return true
+  }
+
+  return false
 }
