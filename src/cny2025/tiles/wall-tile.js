98e397f1568a0cc47d8663669ac45fd47eca5753
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-01-27 02:44:48 +0000

    FloorTile, WallTile: add sprites.

--- src/cny2025/tiles/wall-tile.js
+++ src/cny2025/tiles/wall-tile.js
@@ -8,5 +8,21 @@ export default class WallTile extends Tile {
 
     this.colour = '#808080'
     this.solid = true
+
+    this.spriteSheet = app.assets['cny2025'].img
+    this.spriteSizeX = 16
+    this.spriteSizeY = 16
+    this.spriteScale = 2
+    this.spriteOffsetX = -8
+    this.spriteOffsetY = -8
+  }
+
+  paint (layer = 0) {
+    if (layer === LAYERS.MIDDLE) {
+      this.paintSprite({
+        spriteCol: 7,
+        spriteRow: 0,
+      })
+    }
   }
 }
